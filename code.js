597156d5a4b399539de8c2c3fd6acfc003bb8a84
23-101174-create <23-101174@students.eui.edu.eg>
2025-12-23 03:14:42 +0200

    hero

---
+++ code.js
@@ -0,0 +1,8 @@
+document.getElementById("Home").innerHTML = "Home";
+document.getElementById("Gallery") .innerHTML = "Gallery";
+document.getElementById("Contact-us") .innerHTML = "Contact us";
+document.getElementById("login") .innerHTML = "Login";
+document.getElementById("dark") .innerHTML = "Dark Mode";
+document.getElementById("title") .innerHTML = "Your Voice Matters Your Story Counts";
+document.getElementById("sub-1") .innerHTML = "Bullying can hurt, but speaking up can heal We're here to support you through your journey to mental wellness";
+document.getElementById("button-3") .innerHTML = "Learn More";
